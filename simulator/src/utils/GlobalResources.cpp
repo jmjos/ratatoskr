0a0758a54d55051640f6351161a0665271f88e78
Jan Moritz Joseph <jan.joseph@ovgu.de>
2019-02-14 11:10:39 +0100

    pinpionted

--- simulator/src/utils/GlobalResources.cpp
+++ simulator/src/utils/GlobalResources.cpp
@@ -33,7 +33,7 @@ GlobalResources::GlobalResources()
         synthetic_start_measurement_time(-1)
 {
     rand = new std::mt19937_64();
-    int seed = 4085793378;
+    auto seed = 319189657;
     //auto seed = std::random_device{}();
     rand->seed(seed);
     rd_seed = seed;
