45f103694a2a5f7d9cb77cde240b0f5ee1811acb
Jan Moritz Joseph <jan.joseph@ovgu.de>
2019-02-14 10:32:02 +0100

    trying to reproduce

--- simulator/src/utils/GlobalResources.cpp
+++ simulator/src/utils/GlobalResources.cpp
@@ -33,7 +33,8 @@ GlobalResources::GlobalResources()
         synthetic_start_measurement_time(-1)
 {
     rand = new std::mt19937_64();
-    auto seed = std::random_device{}();
+    int seed = 4085793378;
+    //auto seed = std::random_device{}();
     rand->seed(seed);
     rd_seed = seed;
 }
