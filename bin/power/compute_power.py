397cd21c152adb1490e36e6cb7129b30e83a153d
imadhajjar <imadh2011@gmail.com>
2019-03-08 22:15:35 +0100

    reorganizing power script

--- bin/power/compute_power.py
+++ bin/power/compute_power.py
@@ -23,40 +23,27 @@
 import configparser
 import pandas as pd
 ###############################################################################
-num_of_routers = 0
-num_of_layers = 0
-router_d = []
-###############################################################################
 
 
 def make_list(string):
+    """Make a list of floats out of a string after removing unwanted chars."""
     l1 = string.strip('[] \n')
     l2 = l1.split(',')
     return [float(c.strip()) for c in l2]
 ###############################################################################
 
 
-def get_layer(router_id):
-    global num_of_routers
-    global num_of_layers
+def get_layer(router_id, num_of_routers, num_of_layers):
+    """Get the layer of a router."""
     return router_id // (num_of_routers / num_of_layers)
 ###############################################################################
 
 
-def compute_router_power(row):
-    global router_d
-    router_power = 0.0
-    layer = int(get_layer(row['router_id']))
-    for index, value in enumerate(row):
-        if index != 0:
-            router_power += value * router_d[layer]
-    return router_power
-###############################################################################
-
-
 class PowerCalculator:
-    """ The Power Calculator Class """
+    """The Power Calculator Class."""
+
     def __init__(self, config_path, clocks_path, power_path):
+        """Intitialize the PowerCalculator by providing the necessary files."""
         self.config_path = config_path
         self.clocks_path = clocks_path
         self.power_path = power_path
@@ -69,34 +56,51 @@ class PowerCalculator:
             raise
         self.ni_s = make_list(config['Static']['ni'])
         self.router_s = make_list(config['Static']['router'])
-        global router_d
-        router_d = make_list(config['Dynamic']['router'])
+        self.router_d = make_list(config['Dynamic']['router'])
         for line in f:
             if 'Clock Counts' in line:
                 l1 = line.split(':')[1]
                 self.clocks = make_list(l1)
                 break
-        global num_of_layers
-        global num_of_routers
-        num_of_layers = len(self.clocks)
-        num_of_routers = len(self.df_power)
+        self.num_of_layers = len(self.clocks)
+        self.num_of_routers = len(self.df_power)
 
-    def compute_static_power(self):
+    def get_static_power(self):
+        """Get the static power of all routers."""
         total_sum = 0.0
-        global num_of_layers
-        for i in range(0, num_of_layers):
+        for i in range(0, self.num_of_layers):
             total_sum += self.clocks[i] * (self.ni_s[i] + self.router_s[i])
         return total_sum
 
-    def compute_dynamic_power(self):
-        self.df_power['power'] = self.df_power.apply(lambda row: compute_router_power(row), axis=1)
+    def get_dynamic_power(self):
+        """Get the dynamic power of all routers."""
+        self.df_power['power'] = self.df_power.apply(
+                                                self.get_router_power, axis=1)
         self.df_power.to_csv('power_per_router.csv')
         return self.df_power['power'].sum()
 
+    def get_router_power(self, row):
+        """Get the dynamic power of one row(router)."""
+        router_power = 0.0
+        layer = int(get_layer(row['router_id'], self.num_of_routers,
+                    self.num_of_layers))
+        for index, value in enumerate(row):
+            if index != 0:
+                router_power += value * self.router_d[layer]
+        return router_power
+###############################################################################
 
-config_path = 'config.ini'
-clocks_path = '../../simulator/report.txt'
-power_path = '../../simulator/report_Routers_Power.csv'
-calc = PowerCalculator(config_path, clocks_path, power_path)
-print('Static Power:', calc.compute_static_power())
-print('Dynamic Power:', calc.compute_dynamic_power())
+
+def main():
+    """Entry Point."""
+    config_path = 'config.ini'
+    clocks_path = '../../simulator/report.txt'
+    power_path = '../../simulator/report_Routers_Power.csv'
+    calc = PowerCalculator(config_path, clocks_path, power_path)
+    print('Static Power:', calc.get_static_power())
+    print('Dynamic Power:', calc.get_dynamic_power())
+###############################################################################
+
+
+if __name__ == '__main__':
+    main()
