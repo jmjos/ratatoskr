8a77197a7b82f3824223f70fe6e299a6c9342420
MTz12 <max.tzschoppe@ovgu.de>
2020-05-15 17:22:34 +0100

    modified simulation script

--- scripts/experiments/base1/run_simulation.sh
+++ scripts/experiments/base1/run_simulation.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# skript to run ratatoskr simultion
 
 # update sim:
 	# get ratatoskr from github
@@ -9,66 +8,34 @@
 		# ./build.sh
 	# copy sim
 		# cp ~/ratatoskr/simulation/sim ~/'simualtion_folder'
-	
-# add path to simulation folder
-echo "add path to simulation folder: "
-read SIMDIR
-SIM="/home/tzschoppe/$SIMDIR"
-VAR=true
-while [[ $VAR ]]
+
+python generate_sims.py
+
+for subdir in simdir/Heter*/
 do
-	echo -e "is the path '$SIM' correct? (y/n)"
-	read -n1 answer
-	if [[ $answer == n || $answer == N ]]
-	then
-		echo -e "\nreenter path to simulation folder: "
-		read SIMDIR
-		SIM="/home/tzschoppe/$SIMDIR"
-	else
-		VAR=false
-		break
-	fi
+	echo -e "start simulation"
+	
+	cd $subdir
+
+	echo -e "\nrun configure.py"
+	python configure.py
+	PID=$!
+	wait $PID
+
+	echo -e "\nrun run_urand.py with nohub"
+	#nohup python -u run_urand.py > nohup_output.log 2>&1 &
+	python run_urand.py
+	PID=$!
+	wait $PID
+
+	echo -e "\nrun generate_plots.py"
+	python generate_plots.py
+	PID=$!
+	wait $PID	
+
+	cd $OLDPWD
+
+	echo -e "\nstop simulation"
 done
-
-#SIM="/home/tzschoppe/base2"
-
-SIM1="${SIM##*/}"
-echo -e "\n\nrun simulation $SIM1"
-echo "add correct configuration in config.ini? (y/n)"
-read -n1 answer
-
-if [[ $answer == n || $answer == N ]]
-then
-	vim $SIM/config.ini
-fi
-
-echo -e "\nstart simulation? (y/n)"
-read -n1 answer
-if [[ $answer == n || $answer == N ]]
-then
-	echo ""
-	exit 0	
-fi
-
-#echo -e "\n\nsource source_me.sh"
-cd $SIM/
-#source source_me.sh &
-
-echo -e "\nrun configure.py"
-python configure.py
-PID=$!
-wait $PID
-
-echo -e "\nrun run_urand.py with nohub"
-nohup python -u run_urand.py > nohup_output.log 2>&1 &
-PID=$!
-wait $PID
-
-echo -e "\nrun generate_plots.py"
-python generate_plots.py
-PID=$!
-wait $PID
-
-echo -e "\nsimulation done"
 
 exit 0
