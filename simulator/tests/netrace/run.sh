d3edda178b645b8dc2e2cc44b86d29f427a4b232
jmjos <joseph@ice.rwth-aachen.de>
2020-11-19 08:12:16 +0100

    run

--- simulator/tests/netrace/run.sh
+++ simulator/tests/netrace/run.sh
@@ -11,4 +11,5 @@ cp ../build/sim .
 wget https://www.cs.utexas.edu/~netrace/download/netrace-1.0.tar.bz2
 tar -xf netrace-1.0.tar.bz2
 mv netrace-1.0/testraces/example.tra.bz2 config
+cp config/ntConfig.xml config/config.xml
 ./sim
