b2c6ee77faeb80b60c02e790f95f6c407703584b
jmjos <joseph@ice.rwth-aachen.de>
2020-11-19 08:16:22 +0100

    runsh

--- simulator/tests/netrace/run.sh
+++ simulator/tests/netrace/run.sh
@@ -5,11 +5,9 @@ cd build
 cmake ../../.. -DDEFINE_ENABLE_NETRACE=ON
 make -j
 cd ..
-mkdir rundir
-cd rundir
-cp ../build/sim .
+cp build/sim .
 wget https://www.cs.utexas.edu/~netrace/download/netrace-1.0.tar.bz2
 tar -xf netrace-1.0.tar.bz2
-mv netrace-1.0/testraces/example.tra.bz2 config
+mv netrace-1.0/testraces/example.tra.bz2 config/
 cp config/ntConfig.xml config/config.xml
 ./sim
