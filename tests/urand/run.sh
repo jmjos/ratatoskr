d4734de7092bb99d7b9bf32e28abb38e3b97faef
JM Joseph <jmjos@users.noreply.github.com>
2020-11-19 15:51:05 +0100

    Update run.sh

--- tests/urand/run.sh
+++ tests/urand/run.sh
@@ -2,13 +2,12 @@
 
 mkdir test_runner_8x8
 cd  test_runner_8x8
-ls ../../../simulator
 cp ../../../simulator/sim .
 mkdir config
 cp ../2D-8x8/config.xml config/
 cp ../2D-8x8/network.xml config/
 ./sim
-
+cd ..
 
 mkdir test_runner_4x4
 cd  test_runner_4x4
