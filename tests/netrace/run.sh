5e10b5875820505463e99d433b445f401f96df90
jmjos <joseph@ice.rwth-aachen.de>
2020-11-23 13:37:21 +0100

    netraceCommandLineOptionMode

--- tests/netrace/run.sh
+++ tests/netrace/run.sh
@@ -3,16 +3,16 @@
 mkdir build
 cd build
 cmake ../../../simulator -DDEFINE_ENABLE_NETRACE=ON
-make
+make -j3
 cd ..
 cp build/sim .
 wget https://www.cs.utexas.edu/~netrace/download/netrace-1.0.tar.bz2
 tar -xf netrace-1.0.tar.bz2
 mv netrace-1.0/testraces/example.tra.bz2 config/
 cp config/ntConfig.xml config/config.xml
-./sim
+./sim --simTime 1000 --netraceTraceFile config/example.tra.bz2 --netraceRegion 0
 
 #black sholes mode
 wget https://www.cs.utexas.edu/~netrace/download/blackscholes_64c_simsmall.tra.bz2
-mv config/ntConfig-blacksholes.xml config/ntConfig.xml
-./sim
+#mv config/ntConfig-blacksholes.xml config/ntConfig.xml
+./sim --simTime 1000 --netraceTraceFile blackscholes_64c_simsmall.tra.bz2 
