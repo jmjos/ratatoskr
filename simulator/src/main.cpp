d7a2a5663d693b9781fd52244a1e38e04a098dd3
yangyt96 <yee.yang.tan@rwth-aachen.de>
2021-02-15 14:34:38 +0100

    [style] style formatting for better readability

--- simulator/src/main.cpp
+++ simulator/src/main.cpp
@@ -34,24 +34,28 @@
 
 namespace po = boost::program_options;
 
-int sc_main(int arg_num, char* arg_vec[])
+int sc_main(int arg_num, char *arg_vec[])
 {
-    GlobalResources& globalResources = GlobalResources::getInstance();
-    GlobalReport& globalReport = GlobalReport::getInstance();
-    Report& rep = Report::getInstance();  // database report
-    sleep(1);  // wait for the systemC branding
-    cout << endl << "Ratatoskr 3D-NoC Simulator Copyright(C) 2014-2021" << endl;
+    GlobalResources &globalResources = GlobalResources::getInstance();
+    GlobalReport &globalReport = GlobalReport::getInstance();
+    Report &rep = Report::getInstance(); // database report
+    sleep(1);                            // wait for the systemC branding
+    cout << endl
+         << "Ratatoskr 3D-NoC Simulator Copyright(C) 2014-2021" << endl;
     cout << "   Dr. Jan Moritz Joseph (joseph@ice.rwth-aachen.de) " << endl;
     cout << "This program comes with ABSOLUTELY NO WARRANTY;" << endl;
     cout << "This is free software, and you are welcome to redistribute it" << endl;
-    cout << "under certain conditions. For details see README file." << endl << endl;
+    cout << "under certain conditions. For details see README file." << endl
+         << endl;
     sc_report_handler::set_verbosity_level(SC_DEBUG);
     sc_report_handler::set_actions(SC_ID_INSTANCE_EXISTS_, SC_DO_NOTHING); //disable renaming warnings
 
-    if (arg_num==2) {
+    if (arg_num == 2)
+    {
         globalResources.readConfigFile(arg_vec[1]);
     }
-    else {
+    else
+    {
 #ifndef ENABLE_NETRACE
         std::string config_path = "config/config.xml";
 #endif
@@ -73,41 +77,43 @@ int sc_main(int arg_num, char* arg_vec[])
     po::variables_map vm;
     po::options_description desc("Allowed Options");
 
-    desc.add_options()
-            ("simTime", po::value<std::string>()->default_value(""), "Length of simulation");
-    desc.add_options()
-            ("flitTrace", po::value<bool>()->default_value(false), "Activating flit trace files");
+    desc.add_options()("simTime", po::value<std::string>()->default_value(""), "Length of simulation");
+    desc.add_options()("flitTrace", po::value<bool>()->default_value(false), "Activating flit trace files");
 
 #ifdef ENABLE_NETRACE
-    desc.add_options()
-            ("netraceRegion", po::value<int>()->default_value(2), "Netrace: Region of the simulation, per default the PARSEC's ROI");
-    desc.add_options()
-            ("netraceTraceFile", po::value<std::string>()->default_value(""), "Netrace: Trace file");
-    desc.add_options()
-            ("netraceVerbosity", po::value<std::string>()->default_value("all"), "Netrace: Verbosity Level,"
-                                                                                 "all=info about trace file and about packet generation. "
-                                                                                 "base = info about trace file. none = no output");
+    desc.add_options()("netraceRegion", po::value<int>()->default_value(2), "Netrace: Region of the simulation, per default the PARSEC's ROI");
+    desc.add_options()("netraceTraceFile", po::value<std::string>()->default_value(""), "Netrace: Trace file");
+    desc.add_options()("netraceVerbosity", po::value<std::string>()->default_value("all"), "Netrace: Verbosity Level,"
+                                                                                           "all=info about trace file and about packet generation. "
+                                                                                           "base = info about trace file. none = no output");
 #endif
-    try{
+    try
+    {
         po::store(po::parse_command_line(arg_num, arg_vec, desc), vm);
         po::notify(vm);
-    } catch (po::error& e) {
-        cerr << "ERROR: " << e.what() << endl << endl << desc << endl;
+    }
+    catch (po::error &e)
+    {
+        cerr << "ERROR: " << e.what() << endl
+             << endl
+             << desc << endl;
         return 1;
     }
 
     std::string simTimeString = vm["simTime"].as<std::string>();
-    if (!simTimeString.empty()) {
+    if (!simTimeString.empty())
+    {
         globalResources.simulation_time = std::stoi(simTimeString);
     }
     globalResources.activateFlitTracing = vm["flitTrace"].as<bool>();
 
 #ifdef ENABLE_NETRACE
     std::string netraceTraceFile = vm["netraceTraceFile"].as<std::string>();
-    if (!netraceTraceFile.empty()) {
+    if (!netraceTraceFile.empty())
+    {
         globalResources.netraceFile = netraceTraceFile;
     }
-    if (vm["netraceRegion"].as<int>() <=5 && vm["netraceRegion"].as<int>() >= 0)
+    if (vm["netraceRegion"].as<int>() <= 5 && vm["netraceRegion"].as<int>() >= 0)
         globalResources.netraceStartRegion = vm["netraceRegion"].as<int>();
     else
         globalResources.netraceStartRegion = 2;
@@ -122,22 +128,24 @@ int sc_main(int arg_num, char* arg_vec[])
     std::string config_path = "config/ntConfig.xml";
 #endif
 
-
     std::unique_ptr<NoC> noc = std::make_unique<NoC>("Layer");
     std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
     cout << "Random seed " << globalResources.rd_seed << endl;
-    cout << endl << "Starting Simulation!" << endl;
+    cout << endl
+         << "Starting Simulation!" << endl;
     sc_start(globalResources.simulation_time, SC_NS);
 
-    if (globalResources.outputToFile) {
-        cout << "Generating report of the simulation run into file " << globalResources.outputFileName << " ... " << endl << endl;
+    if (globalResources.outputToFile)
+    {
+        cout << "Generating report of the simulation run into file " << globalResources.outputFileName << " ... " << endl
+             << endl;
         globalReport.reportComplete(globalResources.outputFileName);
         cout << " done." << endl;
     }
     globalReport.reportPerformance(cout);
     cout << "Random seed " << globalResources.rd_seed << endl;
 
-    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()-t1);
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - t1);
     auto secs = std::chrono::duration_cast<std::chrono::seconds>(ms);
     ms -= std::chrono::duration_cast<std::chrono::milliseconds>(secs);
     auto mins = std::chrono::duration_cast<std::chrono::minutes>(secs);
